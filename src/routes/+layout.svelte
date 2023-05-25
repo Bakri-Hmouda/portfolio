5e3138ae56361ba91fffbb0e1637992f2e4aa15e
bakrifly <bakri.hmouda@gmail.com>
2023-05-25 16:13:47 +0300

    simple header and about card

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
 	import '../app.css';
 </script>
 
-<slot />
+<header class="px-4 md:px-16 pt-12">
+	<h1 class="text-2xl">Bakri Hmouda</h1>
+	<p class="text-gray-600 dark:text-gray-400">Frontend web developer</p>
+</header>
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<!-- template -->
+<svelte:head>
+	<title>Bakri Hmouda</title>
+</svelte:head>
+
+<section class="max-w-7xl mt-8 px-4 sm:px-16">
+	<div class="bg-slate-100 dark:bg-gray-700 rounded p-6 drop-shadow-sm md:w-1/2">
+		<h2 class="text-xl capitalize mb-2">about</h2>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita similique accusamus eaque
+			deserunt placeat laudantium voluptatem error, molestias nobis, tenetur sapiente eos incidunt
+			tempore vero numquam dolor, suscipit at velit.
+		</p>
+	</div>
+</section>
