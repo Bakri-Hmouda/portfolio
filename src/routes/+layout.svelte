c3d862d1efeaa32ebbe28dd387f512512026886d
bakrifly <bakri.hmouda@gmail.com>
2023-05-29 19:51:14 +0300

    fixed some responsiveness issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 
-<header class="px-4 mb-8 md:mb-12 md:px-8 lg:px-16 pt-12 max-w-7xl m-auto">
+<header class="m-auto mb-8 max-w-7xl px-4 pt-12 md:mb-12 md:px-8 lg:px-16">
 	<h1 class="text-2xl">Bakri Hmouda</h1>
 	<p class="text-gray-600 dark:text-gray-400">Frontend web developer</p>
 </header>
