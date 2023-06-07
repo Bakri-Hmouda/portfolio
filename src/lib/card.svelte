50a9be5f7eca18fa0da3aa7fc2411824e38ac2c8
bakrifly <bakri.hmouda@gmail.com>
2023-06-07 21:36:19 +0300

    removed unused imports

--- src/lib/card.svelte
+++ src/lib/card.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	// imports
 	import Icon from '@iconify/svelte';
-	import { claim_space } from 'svelte/internal';
 
 	// props
 	export let title = 'untitled';
@@ -34,7 +33,7 @@
 </script>
 
 <!-- template -->
-<div class="rounded bg-gray-200 px-4 pb-6 pt-4 drop-shadow-sm dark:bg-gray-800">
+<div class="rounded bg-gray-200 px-4 pb-6 pt-4 shadow-sm dark:bg-gray-800">
 	<div class="mb-10 flex justify-between">
 		<h4 class="align-text-top text-base font-bold">{title}</h4>
 		<Icon {icon} class="h-6 w-6" />
@@ -42,7 +41,7 @@
 	<span>Level: </span><span>{levels[level].title}</span>
 	<div
 		id="progress-bar"
-		class="mt-1 grid h-1 w-full grid-cols-5 gap-0.5 overflow-hidden rounded bg-gray-400 dark:bg-gray-200"
+		class="mt-1 grid h-1 w-full grid-cols-5 gap-0.5 overflow-hidden rounded bg-gray-400 shadow-inner dark:bg-gray-200"
 	>
 		<!-- render the width dynamically using the inline style tag -->
 		<div class:bg-yellow-600={levels[level].level >= 1} />
