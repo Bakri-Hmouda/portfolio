3df0f2c1e3ab4cb84f5952bea8bdceeaf66323f6
bakrifly <bakri.hmouda@gmail.com>
2023-06-07 21:35:03 +0300

    added glow option to drop shadow

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,14 @@
 export default {
   content: ['./src/**/*.{html,js,ts,svelte}'],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        glow: [
+          "0px 0px 3px #22c55e",
+          "0px 0px 5px #22c55e",
+        ],
+      }
+    },
   },
   plugins: [],
 }
-
